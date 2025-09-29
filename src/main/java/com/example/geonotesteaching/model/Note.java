90c86db994f57cf445838b754b3e8727e935b9a9
Rocío <rluqmon168@g.educaand.es>
2025-09-29 23:22:46 +0200

    Ejercicio A1 terminado

--- src/main/java/com/example/geonotesteaching/model/Note.java
+++ src/main/java/com/example/geonotesteaching/model/Note.java
@@ -7,8 +7,12 @@ import java.time.Instant;
 // en un entorno real usaríamos un UUID.
 public record Note(long id, String title, String content, GeoPoint location, Instant createdAt, Attachment attachment) {
     public Note {
-        if (title == null || title.isBlank()) throw new IllegalArgumentException("title requerido");
-        if (content == null) content = "";
+        if (title == null || title.isBlank() || title.trim().length() < 3) throw new IllegalArgumentException("title requerido con, al menos, 3 caracteres");
+        if(content != null) content = content.trim();
+        if (content == null || content.trim().isEmpty()) content = "-";
+        // Otra opción sería utilizar un operador ternario de la siguiente forma:
+        //
+        // content = (content == null || content.trim().isEmpty()) ? "-" : content.trim();
         if (location == null) throw new IllegalArgumentException("location requerido");
         if (createdAt == null) createdAt = Instant.now();
     }
